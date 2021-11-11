0c0d7a8c51a887be0b38a587bfba5a92eea75f3b
Daniel Semyonov <daniel@dsemy.com>
2021-11-11 14:17:00 +0000

    Add Elevator class

---
+++ elevator.py
@@ -0,0 +1,19 @@
+# The Elevator class represents a single elevator in the building,
+# including all the parameters supplied for elevators in the JSON
+# building description except for the ID (it is used as the key in the
+# dictionary containing the elevators).
+# The floor parameter is updated according the floor the elevator will
+# stop at next.
+
+class Elevator:
+    
+    def __init__(self, speed, minFloor, maxFloor, closeTime,
+                 openTime, startTime, stopTime, floor = 0):
+        self.speed = speed
+        self.minFloor = minFloor
+        self.maxFloor = maxFloor
+        self.closeTime = closeTime
+        self.openTime = openTime
+        self.startTime = startTime
+        self.stopTime = stopTime
+        self.floor = floor
