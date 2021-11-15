d8876d2b637da1d4fac94f61c3e17d07abc660e6
Daniel Semyonov <daniel@dsemy.com>
2021-11-15 13:09:51 +0200

    Read calls from csv input as Call objects

--- Ex1.py
+++ Ex1.py
@@ -3,6 +3,7 @@ import json
 import csv
 
 from elevator import Elevator # Our Elevator object.
+from call import Call
 
 def main(buildingFile, callsFile, outputFile):
     with open(buildingFile) as b:
@@ -23,27 +24,10 @@ def main(buildingFile, callsFile, outputFile):
                                            elevator['_stopTime'])
                  for elevator in buildingData['_elevators']}
 
+    # Each Call is added in the order it appears in the input file.
     with open(callsFile) as c:
         callsReader = csv.reader(c)
-        # Calls are kept in an array since they are sorted
-        # chronologically in the CSV file, and since it will simplify
-        # outputting a CSV file later.
-        calls = [call for call in callsReader]
+        calls = [Call(call) for call in callsReader]
 
-    # Dummy algorithm which assigns calls as they appear
-    # chronologically.
-    amount = len(calls)
-    i = 0
-    while i < amount:
-        for elevator in elevators:
-            calls[i][5] = elevator
-            i += 1
-            if i == amount:
-                break
-        
-    with open(outputFile, 'w') as output:
-        outputWriter = csv.writer(output)
-        outputWriter.writerows(calls)
-            
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], sys.argv[3])
