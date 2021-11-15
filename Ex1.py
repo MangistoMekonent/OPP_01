8d7cdc4638d512883b10d455cf2e97ba30efa129
Daniel Semyonov <daniel@dsemy.com>
2021-11-15 06:09:52 +0200

    Implement a simple algorithm for testing

--- Ex1.py
+++ Ex1.py
@@ -5,8 +5,8 @@ import csv
 from elevator import Elevator # Our Elevator object.
 
 def main(buildingFile, callsFile, outputFile):
-    with open(buildingFile) as building:
-        buildingData = json.load(building)
+    with open(buildingFile) as b:
+        buildingData = json.load(b)
 
     # Assign values gathered from the JSON building description.
     maxFloor = buildingData['_maxFloor']
@@ -23,19 +23,27 @@ def main(buildingFile, callsFile, outputFile):
                                            elevator['_stopTime'])
                  for elevator in buildingData['_elevators']}
 
-    with open(callsFile) as calls:
-        callsReader = csv.reader(calls)
+    with open(callsFile) as c:
+        callsReader = csv.reader(c)
         # Calls are kept in an array since they are sorted
         # chronologically in the CSV file, and since it will simplify
         # outputting a CSV file later.
         calls = [call for call in callsReader]
 
+    # Dummy algorithm which assigns calls as they appear
+    # chronologically.
+    amount = len(calls)
+    i = 0
+    while i < amount:
+        for elevator in elevators:
+            calls[i][5] = elevator
+            i += 1
+            if i == amount:
+                break
+        
     with open(outputFile, 'w') as output:
         outputWriter = csv.writer(output)
-        for call in calls:
-            outputCall = call
-            outputCall[5] = 0
-            outputWriter.writerow(outputCall)
+        outputWriter.writerows(calls)
             
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], sys.argv[3])
