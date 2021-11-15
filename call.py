c842a8bd9ae3dc940878418b394531c00a722472
Daniel Semyonov <daniel@dsemy.com>
2021-11-15 14:06:37 +0200

    Cast numerical parameters of Call objects as integers

--- call.py
+++ call.py
@@ -1,8 +1,8 @@
 class Call:
     def __init__(self, callList):
         self.time = float(callList[1])
-        self.source = callList[2]
-        self.destination = callList[3]
+        self.source = int(lcallList[2])
+        self.destination = int(callList[3])
         if self.source < self.destination:
             self.direction = True
         elif self.source > self.destination:
